68e81164cd3e966fa03b851243593f1c2a227450
Vincent Hanquez <vincent@coretype.io>
2023-12-31 10:56:45 +0800

    add better method to append tokentrees

--- macro-quote-types/src/lib.rs
+++ macro-quote-types/src/lib.rs
@@ -5,56 +5,71 @@ extern crate proc_macro;
 
 pub mod ext;
 
-use alloc::{vec, vec::Vec};
+use alloc::vec::Vec;
 use proc_macro::{Literal, TokenTree};
 
 pub trait ToTokenTrees {
-    fn generate(ty: Self) -> Vec<TokenTree>;
+    fn append_tt(ty: Self, out: &mut Vec<TokenTree>);
+
+    fn generate(ty: Self) -> Vec<TokenTree>
+    where
+        Self: Sized,
+    {
+        let mut out = Vec::new();
+        Self::append_tt(ty, &mut out);
+        out
+    }
 }
 
 impl ToTokenTrees for &str {
-    fn generate(ty: Self) -> Vec<TokenTree> {
-        vec![Literal::string(ty).into()]
+    fn append_tt(ty: Self, out: &mut Vec<TokenTree>) {
+        out.push(Literal::string(ty).into())
     }
 }
 
 impl ToTokenTrees for alloc::string::String {
-    fn generate(ty: Self) -> Vec<TokenTree> {
-        vec![Literal::string(ty.as_str()).into()]
+    fn append_tt(ty: Self, out: &mut Vec<TokenTree>) {
+        out.push(Literal::string(ty.as_str()).into())
     }
 }
 
 impl ToTokenTrees for u32 {
-    fn generate(ty: Self) -> Vec<TokenTree> {
-        vec![Literal::u32_suffixed(ty).into()]
+    fn append_tt(ty: Self, out: &mut Vec<TokenTree>) {
+        out.push(Literal::u32_suffixed(ty).into())
     }
 }
 
 impl ToTokenTrees for usize {
-    fn generate(ty: Self) -> Vec<TokenTree> {
-        vec![Literal::usize_suffixed(ty).into()]
+    fn append_tt(ty: Self, out: &mut Vec<TokenTree>) {
+        out.push(Literal::usize_suffixed(ty).into())
     }
 }
 
 impl ToTokenTrees for Vec<TokenTree> {
-    fn generate(ty: Self) -> Vec<TokenTree> {
-        ty
+    fn append_tt(ty: Self, out: &mut Vec<TokenTree>) {
+        out.extend(ty.into_iter())
+    }
+}
+
+impl ToTokenTrees for &mut Vec<TokenTree> {
+    fn append_tt(ty: Self, out: &mut Vec<TokenTree>) {
+        out.append(ty)
     }
 }
 impl ToTokenTrees for proc_macro::TokenStream {
-    fn generate(ty: Self) -> Vec<TokenTree> {
-        ty.into_iter().collect::<Vec<_>>()
+    fn append_tt(ty: Self, out: &mut Vec<TokenTree>) {
+        out.extend(ty)
     }
 }
 
 impl ToTokenTrees for proc_macro::Literal {
-    fn generate(lit: Self) -> Vec<TokenTree> {
-        vec![lit.into()]
+    fn append_tt(ty: Self, out: &mut Vec<TokenTree>) {
+        out.push(ty.into())
     }
 }
 
 impl ToTokenTrees for proc_macro::Ident {
-    fn generate(ident: Self) -> Vec<TokenTree> {
-        vec![ident.into()]
+    fn append_tt(ty: Self, out: &mut Vec<TokenTree>) {
+        out.push(ty.into())
     }
 }
